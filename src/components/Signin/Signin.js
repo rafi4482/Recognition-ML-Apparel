26d0fb283d630821fdaf8e6db879e399272d9ad2
Md Rafi Uz Zaman <rafi.kazla@gmail.com>
2023-05-28 16:18:36 +0600

    signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { toast, Toaster } from 'react-hot-toast';
+import React, { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
 
 const Signin = ({ onRouteChange, loadUser }) => {
-  const [signInEmail, setSignInEmail] = useState('');
-  const [signInPassword, setSignInPassword] = useState('');
+  const [signInEmail, setSignInEmail] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
 
   const onEmailChange = (event) => {
     setSignInEmail(event.target.value);
@@ -15,72 +15,88 @@ const Signin = ({ onRouteChange, loadUser }) => {
 
   const onSubmitSignIn = async () => {
     try {
-      const response = await fetch('http://localhost:3000/signin', {
-        method: 'post',
-        headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("http://localhost:3000/signin", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           email: signInEmail,
-          password: signInPassword
-        })
+          password: signInPassword,
+        }),
       });
 
       const user = await response.json();
 
       if (response.ok && user.id) {
         loadUser(user);
-        onRouteChange('home');
-        toast.success('Sign-in successful');
+        onRouteChange("home");
+        toast.success("Sign-in successful");
       } else {
-        toast.error('Invalid email or password');
+        toast.error("Invalid email or password");
       }
     } catch (error) {
       console.log(error);
-      toast.error('An error occurred');
+      toast.error("An error occurred");
     }
   };
 
   return (
-    <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-<Toaster />
-      <main className="pa4 black-80">
-        <div className="measure">
-          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+    <>
+      <h1 style={{ marginBottom: "70px" }}>
+        <span style={{ color: "white" }}>Login</span> to unveil the hidden
+        identity of apparels!
+      </h1>
+
+      <article className="mt3 br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <Toaster />
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
               <input
-                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                type="email"
-                name="email-address"
-                id="email-address"
-                onChange={onEmailChange}
+                onClick={onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Sign in"
               />
             </div>
-            <div className="mv3">
-              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input
-                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                type="password"
-                name="password"
-                id="password"
-                onChange={onPasswordChange}
-              />
+            <div className="lh-copy mt3">
+              <p
+                onClick={() => onRouteChange("register")}
+                className="f6 link dim black db pointer"
+              >
+                Register
+              </p>
             </div>
-          </fieldset>
-          <div className="">
-            <input
-              onClick={onSubmitSignIn}
-              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-              type="submit"
-              value="Sign in"
-            />
           </div>
-          <div className="lh-copy mt3">
-            <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-          </div>
-        </div>
-      </main>
-    </article>
+        </main>
+      </article>
+    </>
   );
 };
 
